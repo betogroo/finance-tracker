2f0c8b46c3212d2d54b7738669f1507a8bad216b
betogroo <luizhumberto@gmail.com>
2024-09-25 10:48:20 -0300

    fix: wrap code with nav

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -8,13 +8,15 @@
 
 <template>
   <div>
-    <ul>
-      <li
-        v-for="route in navBarRoutes"
-        :key="route.name"
-      >
-        <a :href="route.path">{{ route.meta.title }}</a>
-      </li>
-    </ul>
+    <nav>
+      <ul>
+        <li
+          v-for="route in navBarRoutes"
+          :key="route.name"
+        >
+          <a :href="route.path">{{ route.meta.title }}</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
