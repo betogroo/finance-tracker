23573e3f820429199d69a402e96db6b635e0f54c
betogroo <luizhumberto@gmail.com>
2024-09-25 15:23:56 -0300

    feat: stylize navbar

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -3,16 +3,28 @@
 </script>
 
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li
-          v-for="route in navBarRoutes"
-          :key="route.name"
-        >
-          <a :href="route.path">{{ route.meta.title }}</a>
+  <nav>
+    <ul class="flex space-x-2 justify-end mr-4">
+      <template
+        v-for="(route, i) in navBarRoutes"
+        :key="route.name"
+      >
+        <li>
+          <ULink
+            class="hover:underline"
+            active-class="font-semibold"
+            :to="route.path"
+          >
+            {{ route.meta.title }}
+          </ULink>
         </li>
-      </ul>
-    </nav>
-  </div>
+        <div
+          v-if="i < navBarRoutes.length - 1"
+          class="space-x-1"
+        >
+          |
+        </div>
+      </template>
+    </ul>
+  </nav>
 </template>
