ab74c219faff3e8652c74441e9380564e2b3afc6
betogroo <luizhumberto@gmail.com>
2024-09-25 16:59:35 -0300

    feat: add avatar

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
   const { navBarRoutes } = useNav()
+  const goToProfile = () => {
+    console.log('Go to profile')
+  }
 </script>
 
 <template>
-  <nav class="flex justify-between items-center bg-white h-8 shadow-sm px-2">
+  <nav class="flex justify-between items-center bg-white h-10 shadow-sm px-2">
     <h1>
       <ULink
         to="/"
@@ -11,27 +14,35 @@
         >Finance Tracker</ULink
       >
     </h1>
-    <ul class="flex space-x-2 justify-end">
-      <template
-        v-for="(route, i) in navBarRoutes"
-        :key="route.name"
-      >
-        <li>
-          <ULink
-            class="hover:underline"
-            active-class="font-semibold"
-            :to="route.path"
-          >
-            {{ route.meta.title }}
-          </ULink>
-        </li>
-        <div
-          v-if="i < navBarRoutes.length - 1"
-          class="space-x-1"
+    <div class="flex justify-around items-center space-x-2">
+      <ul class="flex space-x-2 justify-end">
+        <template
+          v-for="(route, i) in navBarRoutes"
+          :key="route.name"
         >
-          |
-        </div>
-      </template>
-    </ul>
+          <li>
+            <ULink
+              class="hover:underline"
+              active-class="font-semibold"
+              :to="route.path"
+            >
+              {{ route.meta.title }}
+            </ULink>
+          </li>
+          <div
+            v-if="i < navBarRoutes.length - 1"
+            class="space-x-1"
+          >
+            |
+          </div>
+        </template>
+      </ul>
+      <UAvatar
+        @click="goToProfile"
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+        class="cursor-pointer"
+      />
+    </div>
   </nav>
 </template>
